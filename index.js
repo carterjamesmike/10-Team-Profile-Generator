d3c1f4146300cb00b700873fb29fa7902852cf88
James Carter <carterjamesmike@gmail.com>
2022-11-19 20:19:58 -0600

    removed node-html-parser

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
-const parse = require('node-html-parser').parse;
 
 const team = [];
 
